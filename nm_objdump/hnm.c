722824ae9d95ade2eadd165ecbf32f1ad5d06637
PiusEzekiel <e.pius1@alustudent.com>
2024-10-18 10:36:14 +0000

    hnm.c

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void print_symbol(const char *name, Elf64_Sym *sym, char *strtab) {
+void print_symbol(Elf64_Sym *sym, char *strtab) {
     char type;
     switch (ELF64_ST_TYPE(sym->st_info)) {
         case STT_FUNC: type = 'T'; break;
@@ -17,25 +17,35 @@ void print_symbol(const char *name, Elf64_Sym *sym, char *strtab) {
 }
 
 void read_elf(const char *filename) {
-    int fd = open(filename, O_RDONLY);
+    int fd;
+    Elf64_Ehdr ehdr;
+    Elf64_Shdr *shdrs;
+    char *strtab = NULL;
+    int i;
+
+    fd = open(filename, O_RDONLY);
     if (fd < 0) {
         perror("open");
         return;
     }
 
-    Elf64_Ehdr ehdr;
     if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) {
         perror("read");
         close(fd);
         return;
     }
 
+    shdrs = malloc(ehdr.e_shnum * sizeof(Elf64_Shdr));
+    if (shdrs == NULL) {
+        perror("malloc");
+        close(fd);
+        return;
+    }
+
     lseek(fd, ehdr.e_shoff, SEEK_SET);
-    Elf64_Shdr *shdrs = malloc(ehdr.e_shnum * sizeof(Elf64_Shdr));
     read(fd, shdrs, ehdr.e_shnum * sizeof(Elf64_Shdr));
 
-    char *strtab = NULL;
-    for (int i = 0; i < ehdr.e_shnum; i++) {
+    for (i = 0; i < ehdr.e_shnum; i++) {
         if (shdrs[i].sh_type == SHT_STRTAB && i == ehdr.e_shstrndx) {
             strtab = malloc(shdrs[i].sh_size);
             lseek(fd, shdrs[i].sh_offset, SEEK_SET);
@@ -44,14 +54,14 @@ void read_elf(const char *filename) {
         }
     }
 
-    for (int i = 0; i < ehdr.e_shnum; i++) {
+    for (i = 0; i < ehdr.e_shnum; i++) {
         if (shdrs[i].sh_type == SHT_SYMTAB) {
             Elf64_Sym *syms = malloc(shdrs[i].sh_size);
             lseek(fd, shdrs[i].sh_offset, SEEK_SET);
             read(fd, syms, shdrs[i].sh_size);
 
             for (size_t j = 0; j < shdrs[i].sh_size / sizeof(Elf64_Sym); j++) {
-                print_symbol(strtab, &syms[j], strtab);
+                print_symbol(&syms[j], strtab);
             }
             free(syms);
         }
@@ -63,12 +73,14 @@ void read_elf(const char *filename) {
 }
 
 int main(int argc, char *argv[]) {
+    int i;
+    
     if (argc < 2) {
         fprintf(stderr, "Usage: %s [objfile ...]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    for (int i = 1; i < argc; i++) {
+    for (i = 1; i < argc; i++) {
         read_elf(argv[i]);
     }
 
