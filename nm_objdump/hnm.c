4a27b61cb823125065320b2ecf5f5c136e9d3027
piusezekiel <e.pius1@alustudent.com>
2024-10-18 13:11:51 +0200

    hnm

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -6,12 +6,15 @@
 
 void print_symbol(Elf64_Sym *sym, char *strtab) {
     char type;
+    
     switch (ELF64_ST_TYPE(sym->st_info)) {
-        case STT_FUNC: type = 'T'; break;
-        case STT_OBJECT: type = 'D'; break;
-        case STT_NOTYPE: type = ' '; break;
-        default: type = '?'; break;
+        case STT_FUNC:     type = 'T'; break;
+        case STT_OBJECT:   type = 'D'; break;
+        case STT_NOTYPE:   type = ' '; break;
+        case STT_GNU_UNDEFINED: type = 'U'; break;
+        default:           type = '?'; break;
     }
+
 
     printf("%016lx %c %s\n", (unsigned long)sym->st_value, type, &strtab[sym->st_name]);
 }
@@ -43,12 +46,29 @@ void read_elf(const char *filename) {
         return;
     }
 
-    lseek(fd, ehdr.e_shoff, SEEK_SET);
-    read(fd, shdrs, ehdr.e_shnum * sizeof(Elf64_Shdr));
+    if (lseek(fd, ehdr.e_shoff, SEEK_SET) == -1) {
+        perror("lseek");
+        free(shdrs);
+        close(fd);
+        return;
+    }
+
+    if (read(fd, shdrs, ehdr.e_shnum * sizeof(Elf64_Shdr)) != ehdr.e_shnum * sizeof(Elf64_Shdr)) {
+        perror("read");
+        free(shdrs);
+        close(fd);
+        return;
+    }
 
     for (i = 0; i < ehdr.e_shnum; i++) {
         if (shdrs[i].sh_type == SHT_STRTAB && i == ehdr.e_shstrndx) {
             strtab = malloc(shdrs[i].sh_size);
+            if (strtab == NULL) {
+                perror("malloc");
+                free(shdrs);
+                close(fd);
+                return;
+            }
             lseek(fd, shdrs[i].sh_offset, SEEK_SET);
             read(fd, strtab, shdrs[i].sh_size);
             break;
@@ -58,12 +78,21 @@ void read_elf(const char *filename) {
     for (i = 0; i < ehdr.e_shnum; i++) {
         if (shdrs[i].sh_type == SHT_SYMTAB) {
             Elf64_Sym *syms = malloc(shdrs[i].sh_size);
+            if (syms == NULL) {
+                perror("malloc");
+                free(shdrs);
+                free(strtab);
+                close(fd);
+                return;
+            }
+
             lseek(fd, shdrs[i].sh_offset, SEEK_SET);
             read(fd, syms, shdrs[i].sh_size);
 
-		for (j = 0; j < shdrs[i].sh_size / sizeof(Elf64_Sym); j++) {
+            for (j = 0; j < shdrs[i].sh_size / sizeof(Elf64_Sym); j++) {
                 print_symbol(&syms[j], strtab);
             }
+
             free(syms);
         }
     }
