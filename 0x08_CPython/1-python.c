32cabebfbfb0651d6ef854084fe409ab41eae0e8
PiusEzekiel <e.pius1@alustudent.com>
2024-10-26 13:05:41 +0000

    1-python.c

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -18,6 +18,6 @@ void print_python_list(PyObject *p)
 	for (Py_ssize_t i = 0; i < size; i++) {
 		PyObject *item = PyList_GetItem(p, i);
 		const char *type_name = Py_TYPE(item)->tp_name;
-		printf("Element %zd: %s\\n", i, type_name);
+		printf("Element %zd: %s\n", i, type_name);
 	}
 }
